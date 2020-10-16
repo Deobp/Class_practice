5b5189d7c16c9c370cfa58d734122a065fa49c8f
Sasha <zzsanchozz@gmail.com>
2020-10-16 16:35:40 +0300

    solving with constructor functions

--- src/class_practice.js
+++ src/class_practice.js
@@ -1,38 +1,27 @@
-class Person {
-    constructor(firstName, lastName, age, gender, interests) {
-        this.name = `${this.firstName}, ${this.lastName}`;
-        this.age = age;
-        this.gender = gender;
-        this.interests = interests;
+function Person(firstName, lastName, age, gender, interests) {
+    this.name = `${firstName}, ${lastName}`;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
 
-    }
-    bio() {
+    this.bio = () => {
         return `${this.name} is ${this.age} years old. They like ${this.interests}`;
     }
 
-    greeting() {
-        return `Hi! I'm ${this.name}`;
-    }
-
+    this.greeting = () => `Hi! I'm ${this.name}`;
 }
 
-class Teacher extends Person {
-    constructor(firstName, lastName, age, gender, interests, subject) {
-        super(firstName, lastName, age, gender, interests);
-        this.subject = subject;
+function Teacher(firstName, lastName, age, gender, interests, subject) {
+    Person.call(this, firstName, lastName, age, gender, interests);
+    this.subject = subject;
+    this.greeting = function() {
+        return `Hello. My name is ${this.name.split(' ')[0][0]}.${this.name.split(' ')[1]}, and I teach ${this.subject}.`
     }
-
-    greeting() {
-        return `Hello. My name is ${this.name.split(' ')[0][0]}.${this.name.split(' ')[1]}, and I teach ${this.subject}.`;
-    }
-
 }
 
-class Student extends Person {
-    constructor(firstName, lastName, age, gender, interests) {
-        super(firstName, lastName, age, gender, interests);
-    }
-    greeting() {
-        return `Yo! I'm ${this.name,split(' ')[0]}.`;
+function Student(firstName, lastName, age, gender, interests) {
+    Person.call(this, firstName, lastName, age, gender, interests);
+    this.bio = () => {
+        return `Yo! I'm ${this.firstName}.`
     }
 }
